687d69dd3627fabf8ab126afcbcea05ca9759a19
Patrycja Wołosz <patrycjawolosz@MacBook-Pro-Patrycja.local>
2019-12-07 13:36:30 +0100

    maze_runner

---
+++ maze_runner_full_kolizja.py
@@ -0,0 +1,63 @@
+WIDTH = 800
+HEIGHT = 600
+
+RUCH = 5
+CZERWONY = (255,0,0)
+
+alien = Actor('alien', anchor=('center', 'center'), midleft=(30,HEIGHT/2))
+
+alien.coords = alien.center
+
+gorna_sciana = Rect((0,0), (WIDTH, 30))
+dolna_sciana = Rect((0,HEIGHT-30), (WIDTH,30))
+lewa_sciana_gora = Rect((0,0), (30, HEIGHT/2-60))
+lewa_sciana_dol= Rect((0,HEIGHT/2+60), (30, HEIGHT/2-60))
+prawa_sciana_gora = Rect((WIDTH-30, 0), (30, HEIGHT/2-60))
+prawa_sciana_dol = Rect((WIDTH-30, HEIGHT/2+60), (30, HEIGHT/2-60))
+
+sciana_labirynt_1 = Rect((200, 150), (20, 250))
+sciana_labirynt_2 = Rect((400, 300), (20, 250))
+sciana_labirynt_3 = Rect((600, 150), (20, 250))
+
+koniec = Rect((WIDTH-10, HEIGHT/2-60), (10, 120))
+
+sciany = [gorna_sciana, dolna_sciana, lewa_sciana_gora, lewa_sciana_dol, prawa_sciana_gora, prawa_sciana_dol, sciana_labirynt_1, sciana_labirynt_2, sciana_labirynt_3]
+
+def draw():
+    screen.fill('black')
+    for s in sciany:
+        screen.draw.filled_rect(s, CZERWONY)
+    alien.draw()
+
+    if alien.colliderect(koniec):
+        screen.fill('green')
+        screen.draw.text(
+            str("WINNER"),
+            color='white',
+            midtop=(WIDTH // 2, 250),
+            fontsize=100)
+
+def update():
+    lista_zderzen = alien.collidelistall(sciany)
+
+    for i in lista_zderzen:
+        if alien.colliderect(sciany[i]):
+            alien.pos = alien.coords
+
+    alien.coords = alien.pos
+
+    if keyboard.left:
+        alien.poprzedni_x = alien.x
+        alien.x -= RUCH
+
+    if keyboard.right:
+        alien.poprzedni_x = alien.x
+        alien.x += RUCH
+
+    if keyboard.up:
+        alien.poprzedni_y = alien.y
+        alien.y -= RUCH
+
+    if keyboard.down:
+        alien.poprzedni_y = alien.y
+        alien.y += RUCH
